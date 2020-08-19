db5f0430bacd0d352e206dc73b5c9e98e20f8a9d
Adanieljohnson91 <anthonydanjohnson@gmail.com>
2020-08-19 08:32:52 -0500

    EWE Full push

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -3,12 +3,12 @@ import "./page-styles/games.css"
 const ATROCITY_IMG = require('../assets/images/Atrocity_Logo.png');
 const ATROCITY_CONFIDENTIAL = require('../assets/images/Atrocity_FactSheet.png')
 
-const GamesPage = () =>{
+const GamesPage = () => {
     return (
         <div className=" align-content-center ">
-            <img src={ATROCITY_IMG} alt="atrocity field of hands logo, atrocity text with spear"/>
+            <img src={ATROCITY_IMG} alt="atrocity field of hands logo, atrocity text with spear" />
             <div>
-            <img src={ATROCITY_CONFIDENTIAL} className="w-50  center-afoh" alt="Fact sheetm Department of Media Relations Nova Scotia, Canada.
+                <img src={ATROCITY_CONFIDENTIAL} className="w-50  center-afoh" alt="Fact sheetm Department of Media Relations Nova Scotia, Canada.
              Fact Sheet for Atrocity Field of Hands (Ep. 01) 1. Developer: Early Warning Entertainment Inc.
              2. Release Date: 2020.  3. Platforms: Playstation, PC 4. Avalability: Digital Download 5. Website www.atrocitygame.com 6. Anticipated Rating / ESRB Mature 17+ 
              7. Press Contact: intel@earlywarning.ca 8. Social: twitter.com/atrocity_game  facebook.com/atrocitygame 9. Description: Developed by military veterans, Atrocity: Field of Hands
@@ -16,7 +16,10 @@ const GamesPage = () =>{
              Step into a war zone infested by the authenticity that only combat veterans can bring with real world decision points, true-to-life ballistics, and an immersive storyline. Sometimes
              it is better to be tried by tweleve than carried by six. ATROCITY FIELD OF HANDS"/>
             </div>
-            <p className="center-text">Press Kit</p>
+            <a href="https://onedrive.live.com/?id=96DED0AB7D6FDB30%2116943&cid=96DED0AB7D6FDB30"
+                target="_blank">
+                <p className="center-text">Press Kit</p>
+            </a>
         </div>
     )
 }
